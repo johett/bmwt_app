af0bd1eeb78fbec4603c9af0361a89d48bf86d3c
”Johannes <j.hett@arcor.de>
2022-06-26 11:28:58 +0200

    [devjo] add chartdata

--- bmwt_app/lib/utility/ChartData.dart
+++ bmwt_app/lib/utility/ChartData.dart
@@ -1,7 +1,7 @@
 /* this class defines the data format for the bar chart
 */
-class _ChartData {
-  _ChartData(this.x, this.y);
+class ChartData {
+  ChartData(this.x, this.y);
 
 // category axis
   String x;
